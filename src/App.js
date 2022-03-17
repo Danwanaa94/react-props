37336e0843dfce8097293f3af80da09bcd1ce210
Danwanaa94 <raufyussif04@gmail.com>
2022-03-17 12:04:25 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import NavBar from "./components/NavBar";
+import Todo from  "./components/Todo";
 
-function App() {
+function App(props){
+  let title = "My first react app";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+<NavBar title={title} />
+<main>
+<Todo title="Learning react" datetime="23rd March 2022"/>
+<Todo title="Premest training" datetime="2nd April 2022"/>
+<Todo title="Going to market" datetime="23rd May 2022"/>
+<Todo title="Going for an inservice training" datetime="3rd June 2022"/>
+<Todo title="Attending an interview" datetime=" 1st July 2022"/>
+</main>
+
     </div>
   );
 }
